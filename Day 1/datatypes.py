b144b0eff9e05041a9626aa8ee09737fdb06d97b
NithyaShriSK <sknithyashri07@gmail.com>
2025-08-22 23:15:39 +0530

    Initial Commit

---
+++ Day 1/datatypes.py
@@ -0,0 +1,16 @@
+a = 10
+print("Integer:",type(a))
+b = 3.14
+print("Float:",type(b))
+c = "Hello World"
+print("String:",type(c))
+d = True
+print("Boolean:",type(d))
+e = [1, 2, 3, 4, 5]
+print("List:",type(e))
+f = (10, 20, 30)
+print("Tuple:",type(f))
+g = {1, 2, 3, 4, 5}
+print("Set:",type(g))
+h = {"name": "Nithya", "age": 18}
+print("Dictionary:",type(h))
